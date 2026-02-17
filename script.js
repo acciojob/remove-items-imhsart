fb1c12a96ac9d364c0cae54e3a2499601ccb2ebd
Nitin Rana <nitinrana2519@gmail.com>
2026-02-17 17:21:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let removeBtn = document.querySelector('input')
+let selector = document.getElementById('colorSelect')
+removeBtn.addEventListener('click', ()=>{
+	let value = selector.value
+	let option = document.querySelector(`[value=${value}]`)
+	option.remove()
+})
